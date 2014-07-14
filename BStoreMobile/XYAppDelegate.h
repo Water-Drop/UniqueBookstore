8a54bec977f1e9498048f51e14648f2e33475008
Julie <julie@JuliematoMacBook-Pro.local>
2014-07-14 09:00:01 +0800

    Initial Commit

---
+++ BStoreMobile/XYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYAppDelegate.h
+//  BStoreMobile
+//
+//  Created by Julie on 14-7-14.
+//  Copyright (c) 2014年 SJTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BStoreMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BStoreMobile
+//
+//  Created by Julie on 14-7-14.
+//  Copyright (c) 2014年 SJTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYAppDelegate class]));
+    }
+}
