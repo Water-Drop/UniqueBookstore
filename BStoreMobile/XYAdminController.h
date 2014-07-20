672eff8760f1871403d2f05d1c4ffac9a23adae8
Julie <julie@JuliematoMacBook-Pro.local>
2014-07-20 18:13:48 +0800

    segue from 个人中心 to 我的好友 added v1.0

---
+++ BStoreMobile/XYAdminController.h
@@ -0,0 +1,13 @@
+//
+//  XYAdminController.h
+//  BStoreMobile
+//
+//  Created by Julie on 14-7-20.
+//  Copyright (c) 2014年 SJTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYAdminController : UIViewController
+
+@end
---
+++ BStoreMobile/XYDetailTableController.h
@@ -0,0 +1,13 @@
+//
+//  XYDetailTableController.h
+//  BStoreMobile
+//
+//  Created by Julie on 14-7-20.
+//  Copyright (c) 2014年 SJTU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYDetailTableController : UITableViewController
+
+@end
