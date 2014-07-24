d2d31173658da1ae3d5cac2e64b221475433a8ba
Julie <julie@JuliematoMacBook-Pro.local>
2014-07-24 08:56:25 +0800

    Json Parser added v1.0

---
+++ BStoreMobile/XYPubMsgController.m
@@ -0,0 +1,51 @@
+//
+//  XYPubMsgController.m
+//  BStoreMobile
+//
+//  Created by Julie on 14-7-21.
+//  Copyright (c) 2014年 SJTU. All rights reserved.
+//
+
+#import "XYPubMsgController.h"
+
+@interface XYPubMsgController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+
+@end
+
+@implementation XYPubMsgController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
